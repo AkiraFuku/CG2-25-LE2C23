50cb48a5561338c665e059b5def8102340d68f03
AkiraFuku <k024g1002@g.neec.ac.jp>
2025-05-27 10:33:58 +0900

    43p

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -8,10 +8,12 @@ ConstantBuffer<Material> gMaterial : register(b0);
 struct PixelShaderOutput{
     float32_t4 color : SV_TARGET0;
 };
-
+Texture2D<float32_t4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
 PixelShaderOutput main(VertexShaderOutput input)
 {
+    float32_t4 textureColor = gTexture.Sample(gSampler, input.texCoord);
     PixelShaderOutput output;
-    output.color = float32_t4(1.0f, 0.0f, 0.0f, 1.0f); // Red color
+    output.color = gMaterial.Color*textureColor; // Red color
     return output;
 }
