628a9017316a412d6f6484b95c6b7b123eeb3682
AkiraFuku <k024g1002@g.neec.ac.jp>
2025-12-01 14:19:32 +0900

    3Dcommonn

---
+++ project/Object3dcommon.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "d3d12.h"
+#include <wrl/client.h>
+#include <cstdint>
+#include "DXCommon.h"
+class Object3dCommon
+{
+    public:
+    void Initialize(DXCommon* dxCommon);
+    DXCommon* GetDxCommon()const {
+        return dxCommon_;
+    }
+
+    void Object3dCommonDraw();
+
+private:
+    HRESULT hr_;
+
+    DXCommon* dxCommon_;
+
+    //ルートシグネチャ
+    Microsoft::WRL::ComPtr<ID3D12RootSignature>rootSignature_;
+    void CreateRootSignature();
+    //グラフィックパイプラインステート
+    Microsoft::WRL::ComPtr<ID3D12PipelineState> graphicsPipelineState_;
+    void CreatePSO();
+};
+
