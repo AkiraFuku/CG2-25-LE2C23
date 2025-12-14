38641ef8f3c3003e223be4b884bc4cd4f56a8dc9
AkiraFuku <k024g1002@g.neec.ac.jp>
2025-12-14 12:43:39 +0900

    13p

--- project/SrvManager.cpp
+++ project/SrvManager.cpp
@@ -5,3 +5,9 @@ const uint32_t SrvManager::kMaxSRVCount=512;
 void SrvManager::Initialize(DXCommon* dxCommon){
     dxCommon_=dxCommon;
 }
+uint32_t SrvManager::Allocate() {
+    assert(useIndex > kMaxSRVCount);
+    int index = useIndex;
+    useIndex++;
+    return index;
+}
--- project/SrvManager.h
+++ project/SrvManager.h
@@ -8,10 +8,14 @@ public:
     //最大テクスチャ数
     static const uint32_t kMaxSRVCount;
     void Initialize(DXCommon* dxCommon);
+
+    uint32_t Allocate();
+
 private:
     DXCommon* dxCommon_ = nullptr;
     uint32_t descriptorSize_;
     Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> srvHeap_;
 
+    uint32_t useIndex =0;
 };
 
