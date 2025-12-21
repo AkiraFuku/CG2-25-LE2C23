39fbc313caf8a2e0a593acbee54881a78de763a2
AkiraFuku <k024g1002@g.neec.ac.jp>
2025-12-21 14:58:22 +0900

    コンパイラ修正

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -2,14 +2,14 @@
 
 struct Material
 {
-    float32_t4 Color;
-    int32_t enableLighting;
-    float32_t4x4 uvTransform; // UV変換行列
+    float4 Color;
+    int enableLighting;
+    float4x4 uvTransform; // UV変換行列
 };
 struct DirectionalLight
 {
-    float32_t4 color; //ライトの色
-    float32_t3 direction; //ライトの向き
+    float4 color; //ライトの色
+    float3 direction; //ライトの向き
     float intensity; // 明るさ
 
 
@@ -17,14 +17,14 @@ struct DirectionalLight
 ConstantBuffer<Material> gMaterial : register(b0);
 ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
 struct PixelShaderOutput{
-    float32_t4 color : SV_TARGET0;
+    float4 color : SV_TARGET0;
 };
-Texture2D<float32_t4> gTexture : register(t0);
+Texture2D<float4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
 PixelShaderOutput main(VertexShaderOutput input)
 {
-    float4 transformedUV = mul(float32_t4(input.texCoord,0.0f, 1.0f), gMaterial.uvTransform);
-    float32_t4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
+    float4 transformedUV = mul(float4(input.texCoord,0.0f, 1.0f), gMaterial.uvTransform);
+    float4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
     if (textureColor.a < 0.1f)
     {
         discard; // 透明度が低いピクセルを破棄
@@ -38,7 +38,7 @@ PixelShaderOutput main(VertexShaderOutput input)
         float Ndotl = dot(normalize(input.normal), -gDirectionalLight.direction);
         float cos = pow(Ndotl * 0.5f + 0.5f, 2.0f);
         
-        output.color.rgb = gMaterial.Color.rgb * textureColor*gDirectionalLight.color.rgb*cos*gDirectionalLight.intensity;
+        output.color.rgb = gMaterial.Color.rgb * textureColor.rgb*gDirectionalLight.color.rgb*cos*gDirectionalLight.intensity;
         output.color.a = gMaterial.Color.a * textureColor.a;
         
         if (output.color.a < 0.1f)
