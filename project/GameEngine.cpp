ce09487c9866c11eaeb42f3798ec10cff64cbd10
AkiraFuku <k024g1002@g.neec.ac.jp>
2025-12-29 10:56:35 +0900

    ゲームクラス

---
+++ project/GameEngine.cpp
@@ -0,0 +1,7 @@
+#include "GameEngine.h"
+
+
+void GameEngine::Initialize(){};
+void GameEngine::Finalize(){};
+void GameEngine::Update(){};
+void GameEngine::Draw(){}
---
+++ project/GameEngine.h
@@ -0,0 +1,14 @@
+#pragma once
+class GameEngine
+{
+public:
+    void Initialize();
+
+    void Finalize();
+
+    void Update();
+
+    void Draw();
+
+};
+
