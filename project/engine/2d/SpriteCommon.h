ccb4373492f152b971dc8f3ef35a56ba11344ae9
AkiraFuku <k024g1002@g.neec.ac.jp>
2025-12-31 10:49:31 +0900

    スプライトコモン

--- project/engine/2d/SpriteCommon.h
+++ project/engine/2d/SpriteCommon.h
@@ -3,7 +3,7 @@
 #include <wrl/client.h>
 #include <cstdint>
 #include "DXCommon.h"
-
+#include <memory>
 class SpriteCommon
 {
 public:
@@ -21,14 +21,14 @@ public:
 
 private:
     // シングルトンパターン
-    static SpriteCommon* instance;
+    static std::unique_ptr<SpriteCommon> instance;
     SpriteCommon() = default;
     ~SpriteCommon() = default;
     SpriteCommon(const SpriteCommon&) = delete;
     SpriteCommon& operator=(const SpriteCommon&) = delete;
     HRESULT hr_;
 
-    DXCommon* dxCommon_;
+    DXCommon* dxCommon_=nullptr;
 
     //ルートシグネチャ
     Microsoft::WRL::ComPtr<ID3D12RootSignature>rootSignature_;
