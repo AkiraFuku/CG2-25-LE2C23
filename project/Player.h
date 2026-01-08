602e464ae493c2eea3c1cd03fbdfe493d34738e9
toyodayaya <k024g0031@g.neec.ac.jp>
2026-01-08 15:51:22 +0900

    ドリフト処理を実装しました。

--- project/Player.h
+++ project/Player.h
@@ -19,12 +19,39 @@ private:
     // カメラ
     Camera* camera_ = nullptr;
     Transform cameraTransform_;
+    Vector3 targetPos_ = {};
+
+    // 座標補完割合
+    static inline const float kInterpolationRate = 0.1f;
 
     // 行列
     Matrix4x4 worldMatrix_;
 
+    // 角度
+    float driftAngle_ = 0.0f;
+    float angleY_ = 0.0f;
+
     // 速度
     Vector3 velocity_ = {};
+    float speedZ_ = 0.0f;
+    float preSpeedZ_ = 0.0f;
+
+    // 速度減衰率
+    const float kSpeedDecayRate = 0.98f;
+    float driftTimer_ = 0.0f;
+
+    // 加速度
+    const float kAcceleration = 0.02f;
+    float topSpeedZ_ = 0.0f;
+
+    // 最大速度
+    const float kMaxSpeedZ = 1.0f;
+
+    // ドリフト開始フラグ
+    bool isDriftStart_ = false;
+
+    // 加速フラグ
+    bool isAcceleration_ = false;
  
 public:
     // 初期化
@@ -37,6 +64,10 @@ public:
     void Rotate();
     // 移動
     void Move();
+    // カメラ移動
+    void MoveCamera();
+    // ドリフト
+    void Drift();
     // コンストラクタとデストラクタ
     Player() = default;
     ~Player();
